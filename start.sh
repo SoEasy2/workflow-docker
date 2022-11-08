d917a3cfda6bd1010cd72b7121db620a9e89d47c
RomanStass <roman.stasianok@cbitech.ae>
2022-11-08 09:38:43 +0300

    change

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 git clone git@github.com:SoEasy2/workflow-frontend.git -b master
 git clone git@github.com:SoEasy2/workflow-api-users.git -b master
+git clone git@github.com:SoEasy2/workflow-api-gateway.git -b master
+git clone git@github.com:SoEasy2/workflow-api-auth.git -b master
+git clone git@github.com:SoEasy2/workflow-api-mailer.git -b master

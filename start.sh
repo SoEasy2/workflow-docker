bc2dfea0623b6e26c20ecb076b0d0d99908dfcd5
RomanStass <roman.stasianok@cbitech.ae>
2022-11-26 18:17:23 +0300

    Added configuration

--- start.sh
+++ start.sh
@@ -3,3 +3,4 @@ git clone git@github.com:SoEasy2/workflow-api-users.git -b master
 git clone git@github.com:SoEasy2/workflow-api-gateway.git -b master
 git clone git@github.com:SoEasy2/workflow-api-auth.git -b master
 git clone git@github.com:SoEasy2/workflow-api-mailer.git -b master
+git clone git@github.com:SoEasy2/workflow-api-company.git-b master
